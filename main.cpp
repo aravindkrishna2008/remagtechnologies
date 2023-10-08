0910cb4ffeb1c4046c9711debe5b54fb2d25286b
aravindkrishna2008 <aravindkrishna.nambi@gmail.com>
2023-10-08 10:13:21 -0700

    changed main.cpp to current alg

--- main.cpp
+++ main.cpp
@@ -1,61 +1,76 @@
-#include <iostream>
-#include <fstream>
-#include <vector>
 #include <cmath>
-#include <utility>
+#include <fstream>
+#include <iostream>
+#include <vector>
 
 using namespace std;
 
-vector<pair<int,float>> outside_input() {
-    ifstream file;
-      file.open("output.txt");
-    vector<pair<int,float>> raw_data_nT;
-    int data_points;
-    file >> data_points;
-    for(int i = 0; i < data_points; i++) {
-        int n1;
-        float n2;
-        file >> n1 >> n2;
-        raw_data_nT.push_back(make_pair(n1,n2));
-    }
-    return raw_data_nT;
-}
+const double µ_0 =
+    4 * M_PI * 1e-7; // Magnetic permeability of free space (SI units)
 
-//Reconnection Rate
-pair<float,bool> rate(float B) {
-    //physical variables
-    bool magnetic_flip = false;
-    if (B<0) {
-        magnetic_flip = true;
-        B = -B;
-    }
-    float L_times_10e16 = 1.3981973;
-    double µ_0 = 4*M_PI/10000000;
-    float dif = 610;
-    float ion_density = 20000;
-    double v_a = B/sqrt(µ_0*ion_density);
-    float S = v_a*L_times_10e16/dif;
-    return make_pair(100*pow(S,-0.5),magnetic_flip);
+// Function to calculate the magnetic reconnection rate
+pair<double, bool> rate(double B_z) {
+  // Define physical constants
+  double L = 1.0;            // Typical length scale (adjust as needed)
+  double ion_density = 2e19; // Ion density (SI units)
+
+  // Ensure B_z is positive for calculations
+  double abs_B_z = abs(B_z);
+
+  // Calculate Sweet-Parker reconnection rate
+  double v_a = abs_B_z / sqrt(µ_0 * ion_density); // Alfven velocity
+  double S = L * v_a / µ_0;
+
+  double reconnection_rate = 1e-6 * sqrt(S); // Adjust scaling as needed
+
+  // Determine if there's a magnetic flip
+  bool magnetic_flip = (B_z < 0);
+
+  return make_pair(reconnection_rate, magnetic_flip);
 }
 
 int main() {
-    //Reconnection Rate computation using the Sweet-Parker model
-    vector<pair<int,float>> data = outside_input();
-    int month,year;
-    //tracks how long conspicuous activity has occurred
-    int counter = 0;
-    for (auto i : data) {
-        pair<float,bool> R = rate(1000*i.second);
-        i.second = R.first;
-        //index -- reconnection rate (m^2/s) -- IMF flip (yes/no)
-        cout << i.first << " -- " << i.second << " -- " << R.second << "\n";
-        if (R.second && i.second<5000000) {
-            cout << " ** Major IMF flip occurred" << "\n";
-            counter++;
-            if (counter>4) {
-                cout << "\n\n --- POTENTIAL SEVERE SOLAR EVENT --- " << month << "/" << i.first << "/" << year << "\n\n";
-            }
-        }
+  // Read data from the input file
+  ifstream file("output.txt");
+  if (!file.is_open()) {
+    cerr << "Error: Could not open input file 'output.txt'" << endl;
+    return 1;
+  }
+
+  int amt;
+  file >> amt;
+  vector<pair<float, float>> data;
+
+  for (int i = 0; i < amt; i++) {
+    float day, B_z;
+    file >> day >> B_z;
+    data.push_back(make_pair(day, B_z));
+  }
+
+  file.close();
+
+  // Calculate reconnection rates and check for flips
+  int consecutive_flips = 0;
+
+  for (const auto &entry : data) {
+    pair<double, bool> result = rate(entry.second);
+
+    cout << "Day " << entry.first << " - Reconnection Rate: " << result.first
+         << " m^2/s";
+
+    if (result.second) {
+      cout << " - Magnetic Flip: Yes";
+      consecutive_flips++;
+      if (consecutive_flips >= 3) {
+        cout << " *** POTENTIAL SEVERE SOLAR EVENT ***";
+      }
+    } else {
+      cout << " - Magnetic Flip: No";
+      consecutive_flips = 0; // Reset consecutive flip counter
     }
-    //forward data to the exterior programs in the website/backend
+
+    cout << endl;
+  }
+
+  return 0;
 }
